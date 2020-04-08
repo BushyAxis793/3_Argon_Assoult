85c9bd3931cee21a036da0724d83077efaee5324
Mikołaj Nawrocki <m.nawrocki793@gmail.ocm>
2020-04-08 13:07:01 +0200

    UI added
    105. Add Simple Score UI

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,21 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
 
-	void OnParticleCollison(GameObject other)
-	{
-		Destroy(gameObject);
-	}
+
+
+    void OnParticleCollison(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
 }
